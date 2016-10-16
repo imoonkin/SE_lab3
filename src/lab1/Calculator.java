2cf7c86b75c995e5929d7f7a44b047669f22b097
iwarriorgit <warrior_xr@126.com>
2016-10-16 14:27:33 +0800

    second

--- src/lab1/Calculator.java
+++ src/lab1/Calculator.java
@@ -1,4 +1,4 @@
-package com;
+package lab1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,16 +9,20 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Main {
+public class Calculator {
 	public static Pattern p = Pattern.compile("\\+|\\-|\\*|\\^|[a-zA-Z]+|\\d+|\\(|\\)");
 	public static Pattern pnum = Pattern.compile("\\d+");
 	public static Pattern pmul = Pattern.compile("\\*");
 	public static Pattern palph = Pattern.compile("[a-zA-Z]+");
 	public static Pattern pop = Pattern.compile("\\+|\\-|\\*|\\^");
 	public static Pattern pil = Pattern.compile("[^\\+\\-\\*\\^a-zA-Z0-9\\(\\)\\s]");
-	
-	public static List<String> split(String str, List<Boolean>index_neg) {
-		StringBuffer s = new StringBuffer(str);;
+
+	/*
+	 * 按+/-分解表达式， 在index_neg中记录符号的位置， 返回分解表达式
+	 */
+	public static List<String> split(String str, List<Boolean> index_neg) {
+		StringBuffer s = new StringBuffer(str);
+		;
 		List<String> re = new LinkedList<String>();
 		int plus = s.indexOf("+");
 		int minus = s.indexOf("-");
@@ -48,7 +52,11 @@ public class Main {
 		re.add(new String(s));
 		return re;
 	}
-	public static String refactor(String exp){
+
+	/*
+	 * 将表达式重构，包括将数字放置左侧，合并同类项， 返回重构结果
+	 */
+	public static String refactor(String exp) {
 		String re = "";
 		List<Boolean> index_neg = new ArrayList<Boolean>();
 		List<String> exps = split(exp, index_neg);
@@ -82,12 +90,19 @@ public class Main {
 			String i = num;
 			if (!sym.isEmpty()) {
 				for (String k : sym.keySet()) {
-					if (sym.get(k) != 1) {
-						i += "*" + k + "^" + sym.get(k);
+					if (!i.isEmpty()) {
+						if (sym.get(k) != 1) {
+							i += "*" + k + "^" + sym.get(k);
+						} else {
+							i += "*" + k;
+						}
 					} else {
-						i += "*" + k;
+						if (sym.get(k) != 1) {
+							i = k + "^" + sym.get(k);
+						} else {
+							i = k;
+						}
 					}
-						
 				}
 			}
 			if (re.isEmpty()) {
@@ -101,56 +116,76 @@ public class Main {
 			}
 		}
 		return re;
-		
+
 	}
-	
-	public static List<String> generate_items(String str) {
+
+	/*
+	 * 表达式合法性检查，并补充省略的乘法符号 返回补充结果
+	 */
+	public static String generate_items(String str) {
 		Matcher m = p.matcher(str);
-        Matcher mil = pil.matcher(str);
-        List<String> exp = new LinkedList<String>();
-        
-        if (mil.find()) {
-        	System.out.println("illegal charaters");
-    		System.exit(1);
-        }
-        while(m.find()) {
-        	exp.add(m.group());
-        }
-        for (int i = 0; i < exp.size() - 1; i += 1) {
-        	if ((pnum.matcher(exp.get(i)).matches() && (palph.matcher(exp.get(i+1)).matches() || exp.get(i+1).equals("(")))
-        		|| (palph.matcher(exp.get(i)).matches() && (pnum.matcher(exp.get(i+1)).matches() || exp.get(i+1).equals("(")))) {
-        		exp.add(i+1, new String("*"));
-        	}
-        	if ((exp.get(i).equals("^") && !pnum.matcher(exp.get(i+1)).matches())
-        		|| (pop.matcher(exp.get(i)).matches() && (pop.matcher(exp.get(i+1)).matches() 
-        				|| exp.get(i+1).equals(")")))
-        		|| (exp.get(i).equals("(") && pop.matcher(exp.get(i)).matches())
-        		|| pop.matcher(exp.get(0)).matches()) {
-        		System.out.println("invalid expression");
-        		System.exit(1);
-        	}
-        }
-        return exp;
+		Matcher mil = pil.matcher(str);
+		List<String> exp = new LinkedList<String>();
+
+		if (mil.find()) {
+			System.out.println("illegal charaters");
+			System.exit(1);
+		}
+		while (m.find()) {
+			exp.add(m.group());
+		}
+		for (int i = 0; i < exp.size() - 1; i += 1) {
+			if ((pnum.matcher(exp.get(i)).matches()
+					&& (palph.matcher(exp.get(i + 1)).matches() || exp.get(i + 1).equals("(")))
+					|| (palph.matcher(exp.get(i)).matches()
+							&& (pnum.matcher(exp.get(i + 1)).matches() || exp.get(i + 1).equals("(")))) {
+				exp.add(i + 1, new String("*"));
+			}
+			if ((exp.get(i).equals("^") && !pnum.matcher(exp.get(i + 1)).matches())
+					|| (pop.matcher(exp.get(i)).matches()
+							&& (pop.matcher(exp.get(i + 1)).matches() || exp.get(i + 1).equals(")")))
+					|| (exp.get(i).equals("(") && pop.matcher(exp.get(i)).matches()) || exp.get(0) == "*"
+					|| exp.get(0) == "^") {
+				System.out.println("invalid expression");
+				System.exit(1);
+			}
+		}
+		StringBuffer s = new StringBuffer();
+		for (String e : exp) {
+			s.append(e);
+		}
+		return s.toString();
 	}
-	
+
+	/* 返回表达式中全部变量 */
 	public static List<String> all_sym(String str) {
 		Matcher m = palph.matcher(str);
-        List<String> sym = new ArrayList<String>();
-        while(m.find()) {
-        	sym.add(m.group());
-        }
-        return sym;
+		List<String> sym = new ArrayList<String>();
+		while (m.find()) {
+			sym.add(m.group());
+		}
+		return sym;
 	}
-	
+
+	/* 拆分表达式并返回结果 */
+	public static List<String> toList(String str) {
+		Matcher m = p.matcher(str);
+		List<String> exp = new LinkedList<String>();
+		while (m.find()) {
+			exp.add(m.group());
+		}
+		return exp;
+	}
+
+	/* 主函数负责对输入操作的判断与执行 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str = "3+2*x+x*y*3*x*z";
-		
-		System.out.println("Enter 'q' to quit.");
+
+		System.out.println("Enter 'q' to quit\n");
 		Scanner in = new Scanner(System.in);
 		Node tree = null;
-        Map<String, String> symbol_table = new HashMap<String, String>();
-        List<String> all_sym = new ArrayList<String>();
+		Map<String, String> symbol_table = new HashMap<String, String>();
+		List<String> all_sym = new ArrayList<String>();
 		while (true) {
 			str = in.nextLine();
 			if (str.equals("q")) {
@@ -158,15 +193,16 @@ public class Main {
 			} else if (str.startsWith("!simplify")) {
 				String[] str2 = str.split(" ");
 				for (int i = 1; i < str2.length; i += 1) {
-					String[] sim =  str2[i].split("=");
+					String[] sim = str2[i].split("=");
 					symbol_table.put(sim[0], sim[1]);
 				}
-		        String r1 = tree.calculate();
-		        Node sim_tree = new Node(generate_items(r1));
-		        sim_tree.expression();
-		        String r2 = sim_tree.calculate(); 
-		        System.out.println(r2);
-		        symbol_table.clear();
+				String r1 = tree.calculate();
+				// System.out.println(r1);
+				Node sim_tree = new Node(toList(refactor(generate_items(r1))));
+				sim_tree.expression();
+				String r2 = sim_tree.calculate();
+				System.out.println(r2);
+				symbol_table.clear();
 			} else if (str.startsWith("!d/d")) {
 				String dsym = str.substring(4, str.length());
 				if (!all_sym.contains(dsym)) {
@@ -174,17 +210,17 @@ public class Main {
 				} else {
 					Node.set_dsym(dsym);
 					String r1 = tree.derivative();
-				    Node sim_tree = new Node(generate_items(r1));
-				    sim_tree.expression();
-				    String r2 = sim_tree.calculate(); 
-				    System.out.println(r2);
+					Node sim_tree = new Node(toList(refactor(generate_items(r1))));
+					sim_tree.expression();
+					String r2 = sim_tree.calculate();
+					System.out.println(r2);
 				}
 			} else {
 				all_sym = all_sym(str);
-				tree = new Node(generate_items(refactor(str)));
+				tree = new Node(toList(refactor(generate_items(str))));
 				Node.set_table(symbol_table);
 				tree.expression();
 			}
-		}   
+		}
 	}
 }
---
+++ src/lab1/Node.java
@@ -0,0 +1,201 @@
+package lab1;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.Math.pow;
+
+public class Node {
+	// 节点内的表达式
+	private List<String> s;
+	// 左侧表达式，右侧表达式
+	public Node left = null, right = null;
+	// 节点的运算符 u为叶节点
+	private char sym = 'u';
+	// 节点是否由纯数字组成
+	private boolean isDigital = false;
+	// 变量的代入值
+	static private Map<String, String> symbol_table;
+	// 操作符
+	static private String[] op = { "+", "-", "*" };
+	// 待求导变量
+	static private String dsym = "";
+
+	/* 构造节点 */
+	public Node(List<String> s) {
+		this.s = s;
+	}
+
+	/* 设置变量映射表 */
+	static public void set_table(Map<String, String> table) {
+		symbol_table = table;
+	}
+
+	/* 设置待求导变量 */
+	static public void set_dsym(String sym) {
+		dsym = sym;
+	}
+
+	/*
+	 * 将list合并成string， 返回合并结果
+	 */
+	static public String list_to_string(List<String> exp) {
+		StringBuffer re = new StringBuffer();
+		for (String i : exp) {
+			re.append(i);
+		}
+		return re.toString();
+	}
+
+	/* 构造语法树 */
+	public void expression() {
+		for (String i : op) {
+			int p = s.lastIndexOf(i);
+			if (p != -1) {
+				sym = i.charAt(0);
+				left = new Node(s.subList(0, p));
+				right = new Node(s.subList(p + 1, s.size()));
+				left.expression();
+				right.expression();
+				return;
+			}
+		}
+		int p = s.lastIndexOf("^");
+		if (p != -1) {
+			sym = '^';
+			left = new Node(s.subList(0, 1));
+			right = new Node(s.subList(2, 3));
+			return;
+		}
+	}
+
+	/*
+	 * 计算表达式的值 返回结果
+	 */
+	public String calculate() {
+		if (sym == 'u') {
+
+			if (symbol_table.containsKey(s.get(0))) {
+				this.isDigital = true;
+				return symbol_table.get(s.get(0));
+
+			} else {
+				if (s.get(0).matches("\\d+")) {
+					this.isDigital = true;
+				}
+				return s.get(0);
+			}
+		}
+		String re_left = left.calculate();
+		String re_right = right.calculate();
+		if (sym == '^') {
+			if (left.isDigital && right.isDigital) {
+				this.isDigital = true;
+				return String.valueOf(((int) pow(Integer.parseInt(re_left), Integer.parseInt(re_right))));
+			} else {
+				return re_left + "^" + re_right;
+			}
+		} else if (sym == '*') {
+			if (left.isDigital && right.isDigital) {
+				this.isDigital = true;
+				return String.valueOf(Integer.parseInt(re_left) * Integer.parseInt(re_right));
+			} else {
+				return new String(re_left + "*" + re_right);
+			}
+		} else if (sym == '+') {
+			if (left.isDigital && right.isDigital) {
+				this.isDigital = true;
+				return String.valueOf(Integer.parseInt(re_left) + Integer.parseInt(re_right));
+			} else {
+				return re_left + "+" + re_right;
+			}
+		} else if (sym == '-') {
+			if (left.isDigital && right.isDigital) {
+				this.isDigital = true;
+				return String.valueOf(Integer.parseInt(re_left) - Integer.parseInt(re_right));
+			} else {
+				return re_left + "-" + re_right;
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * 求导表达式 返回求导结果
+	 */
+	public String derivative() {
+		String re = derivative2();
+		if (re.isEmpty()) {
+			return "0";
+		} else {
+			return re;
+		}
+	}
+
+	/*
+	 * 求导表达式 返回求导结果
+	 */
+	private String derivative2() {
+		if (sym == 'u') {
+			if (s.get(0).equals(dsym)) {
+				return "1";
+			}
+			return "";
+		}
+		if (sym == '^') {
+			if (s.get(0).equals(dsym)) {
+				int index = Integer.parseInt(right.s.get(0));
+				if (index > 2) {
+					return Integer.toString(index) + "*" + left.s.get(0) + "^" + Integer.toString(index - 1);
+				} else if (index == 2) {
+					return "2*" + left.s.get(0);
+				} else {
+					return "1";
+				}
+			} else {
+				return "";
+			}
+		}
+		String re_left = left.derivative2();
+		String re_right = right.derivative2();
+		if (sym == '*') {
+			String s1, s2;
+			s1 = re_left + "*" + list_to_string(right.s);
+			s2 = list_to_string(left.s) + "*" + re_right;
+			if (re_left.isEmpty()) {
+				s1 = "";
+			} else if (re_left.equals("1")) {
+				s1 = list_to_string(right.s);
+			}
+			if (re_right.isEmpty()) {
+				s2 = "";
+			} else if (re_right.equals("1")) {
+				s2 = list_to_string(left.s);
+			}
+			if (!s1.isEmpty() && !s2.isEmpty()) {
+				return s1 + "+" + s2;
+			} else {
+				return s1 + s2;
+			}
+		} else if (sym == '+') {
+			String s1, s2;
+			s1 = re_left;
+			s2 = re_right;
+			if (!s1.isEmpty() && !s2.isEmpty()) {
+				return s1 + "+" + s2;
+			} else {
+				return s1 + s2;
+			}
+		} else if (sym == '-') {
+			String s1, s2;
+			s1 = re_left;
+			s2 = re_right;
+			if (!s1.isEmpty() && !s2.isEmpty()) {
+				return s1 + "-" + s2;
+			} else {
+				return s1 + s2;
+			}
+		}
+		return null;
+	}
+}
